cf2e15d9b1c5287acd2e0633a6428ebfd518796c
badjateamisha <amishabadjate175@gmail.com>
2022-09-05 12:26:12 +0530

    [Amisha]Added update notes component

---
+++ src/app/components/updatenote/updatenote.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, Inject,Output,EventEmitter } from '@angular/core';
+import { NoteService } from 'src/app/services/noteService/note.service';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-updatenote',
+  templateUrl: './updatenote.component.html',
+  styleUrls: ['./updatenote.component.scss']
+})
+export class UpdatenoteComponent implements OnInit {
+  @Output() noteUpdated = new EventEmitter<any>();
+
+  title: any
+  description: any
+  id: any
+
+  constructor(private noteService: NoteService,public dialogRef: MatDialogRef<UpdatenoteComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { 
+    this.title=data.title;
+    this.description=data.description;
+    this.id=data.id;
+  }
+
+  ngOnInit(): void {
+  }
+  onClose() {
+    let reqData = {
+      Title: this.title,
+      Description: this.description,
+    }
+    console.log('updated', reqData, this.id);
+
+    this.noteService.updatenote( reqData,this.id).subscribe((response: any) => {
+      console.log("Update ",response);
+      this.title=''
+      this.description=''
+      this.noteUpdated.emit(response);
+
+    });
+    this.dialogRef.close();
+   
+  }
+} 
